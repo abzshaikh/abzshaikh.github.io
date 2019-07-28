93e4389bd0c45fd033c6106760ab5b363882cdb9
abzshaikh <53226273+abzshaikh@users.noreply.github.com>
2019-07-28 13:30:47 +0530

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     console.log(finalvalue);
     console.log("http://image.tmdb.org/t/p/original/"+finalvalue.poster_path);
     $(".main-detail-banner").css({"background":"linear-gradient(to right,#000000,#929292),url(http://image.tmdb.org/t/p/w185/"+finalvalue.backdrop_path,"background-repeat":"no-repeat","background-size":"cover","background-blend-mode":"hard-light"});
-    $(".main-detail-banner").css({"background":"url(http://image.tmdb.org/t/p/w185/"+finalvalue.backdrop_path});
+    // $(".main-detail-banner").css({"background":"url(http://image.tmdb.org/t/p/w185/"+finalvalue.backdrop_path});
     $(".overview-content").html(finalvalue.overview);
     $("#overview-img").attr("src","http://image.tmdb.org/t/p/w185/"+finalvalue.poster_path);
     $(".movie-name").html(finalvalue.original_title);
