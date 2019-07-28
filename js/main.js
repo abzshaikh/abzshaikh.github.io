4aa7d37ff9fbae94e513820c410da3a611d7f9ed
abzshaikh <53226273+abzshaikh@users.noreply.github.com>
2019-07-28 14:07:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ $(document).ready(function(){
             slidesToShow: 6,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 3000,
+            autoplaySpeed: 2000,
             responsive: [
             {
                 breakpoint: 1100,
